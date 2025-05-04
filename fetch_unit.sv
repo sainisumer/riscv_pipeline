0390e140f59569524100401c8bcf4125802a7992
sainisumer <sumersaini030@gmail.com>
2025-05-04 13:18:45 +0530

    Update fetch_unit.sv

--- fetch_unit.sv
+++ fetch_unit.sv
@@ -1,1 +1,11 @@
-module fetch_unit(input clk,rst
+module fetch_unit(input clk,rst,input [4:0] pc_srcE,pc_targetE,output [31:0] instrD,output [4:0] pcD);
+
+//instruction memory 
+  instruction_memory im(.clk(clk),.reset(rst),.pc(pc),.instruction_code(instr));
+
+// program counter
+  pc pc(.clk(clk),..reset(rst),.pc(pc);
+  
+  
+
+  
