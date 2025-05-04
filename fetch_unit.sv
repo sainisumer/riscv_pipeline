8b0fe88888c7d75603cbeac62c8afdd7013aa438
sainisumer <sumersaini030@gmail.com>
2025-05-04 13:58:11 +0530

    Update fetch_unit.sv

--- fetch_unit.sv
+++ fetch_unit.sv
@@ -1,10 +1,30 @@
 module fetch_unit(input clk,rst,input [4:0] pc_srcE,pc_targetE,output [31:0] instrD,output [4:0] pcD);
-
+// internal wires
+  wire [4:0] pc,pcD,pc_srcE;
+  wire pc_targetE;
+// pipeline registers for instrD, pcD
+  reg [31:0] instrDreg;
+  reg [4:0] pcregD;
 //instruction memory 
   instruction_memory im(.clk(clk),.reset(rst),.pc(pc),.instruction_code(instr));
 
 // program counter
   pc pc(.clk(clk),..reset(rst),.pc(pc);
+// logic to drive instr and pc to registers
+
+        always@(posedge clk)
+          begin
+            if(!rst)
+              begin
+                  instrDreg <= 0;
+                  pcregD <= 0 ;
+              end
+            else
+                begin
+                    instrDreg <= instr;
+                    pcregD <= pc;
+                end    
+          end
   
   
 
